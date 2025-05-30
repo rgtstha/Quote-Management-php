10543cef00f8c2a4b2204d6460b25e6b156ed083
Ranjit Shrestha <rangeetshrestha12@gmail.com>
2025-05-30 07:56:31 +0545

    list quote completed

--- admin/quote-list.php
+++ admin/quote-list.php
@@ -7,11 +7,13 @@ $stmt =  mysqli_prepare($conn, $query);
 mysqli_execute($stmt);
 $mysqli_result =  mysqli_stmt_get_result($stmt);
 
-echo '<pre>';
-// var_dump($mysqli_result);
-// var_dump(mysqli_fetch_all($mysqli_result));
-echo '</pre>';
+$datas = [];
 
+
+for ($i = 0; $i < mysqli_num_rows($mysqli_result); $i++) {
+    $data =  mysqli_fetch_assoc($mysqli_result);
+    $datas[] = $data;
+}
 
 
 ?>
@@ -33,26 +35,50 @@ echo '</pre>';
                 <th scope="col" class="px-6 py-3">
                     Author Name
                 </th>
-
             </tr>
         </thead>
         <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+            <?php for ($j = 0; $j < count($datas); $j++):
+                $item = $datas[$j];
+            ?>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                    <td class="px-6 py-4">
+                        <?php echo $j + 1; ?>
+                    </td>
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <?php echo $item['quote'] ?>
+                    </th>
+                    <td class="px-6 py-4">
+                        <?php echo $item['author_name']; ?>
+                    </td>
 
-                <td class="px-6 py-4">
-                    Silver
-                </td>
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Apple MacBook Pro 17"
-                </th>
+                </tr>
+            <?php endfor; ?>
 
-                <td class="px-6 py-4">
-                    Laptop
-                </td>
 
-            </tr>
-            
-            
+
+
         </tbody>
     </table>
 </div>
+
+<?php
+
+
+// $numbers = [1, 2, 3, 4];
+
+// $quote = [
+    
+//     [
+//         "quote" => "morning shows the day",
+//         "author_name" => "John"
+//     ]
+// ];
+
+// $item =  [
+//         "quote" => "morning shows the day",
+//         "author_name" => "John"
+// ];
+
+
+// $item["quote"];
