26ae03af88a4ab388dc5f6639bce8bbf7c942180
Ranjit Shrestha <rangeetshrestha12@gmail.com>
2025-06-13 06:48:07 +0545

    added image upload

--- admin/add-quote-handle.php
+++ admin/add-quote-handle.php
@@ -7,17 +7,25 @@ if($_SERVER['REQUEST_METHOD'] != 'POST'){
 
 $quote = $_POST['quote'];
 $author_name = $_POST['author'];
+$author_image = $_FILES["author_image"];
+$image_path = "";
 
+if(isset($author_image) && $author_image['error'] == 0){
+    $upload_dir = "uploads/";
+    $image_name = time() . '_' . basename($author_image['name']);
+    $target_path = $upload_dir . $image_name;
 
-
+    if(move_uploaded_file($author_image['tmp_name'], $target_path )){
+        $image_path  = $target_path;
+    }
+}
 
 include 'db-conn.php';
 
 
-
-$query = "INSERT INTO quote (quote, author_name) VALUES (?, ?)";
+$query = "INSERT INTO quote (quote, author_name, author_image) VALUES (?, ?, ?)";
 $abc = mysqli_prepare($conn, $query);
-mysqli_stmt_bind_param($abc, "ss", $quote, $author_name);
+mysqli_stmt_bind_param($abc, "sss", $quote, $author_name, $image_path);
 mysqli_stmt_execute($abc);
 
 
--- admin/add-quote.php
+++ admin/add-quote.php
@@ -1,7 +1,7 @@
 
 <h1 class="text-4xl font-semibold">Add Quote</h1>
 
-<form class="mt-10" method="POST" action="add-quote-handle.php">
+<form class="mt-10" method="POST" action="add-quote-handle.php" enctype="multipart/form-data">
     <div class="mb-5">
         <label for="quote" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Quote</label>
         <input type="text" name="quote" id="quote" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required />
@@ -10,6 +10,11 @@
         <label for="author" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Author Name</label>
         <input type="text" name="author" id="author" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
     </div>
+    <div class="mb-5">
+        <label for="author-image" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Author Image</label>
+        <input type="file" accept="image/*"  name="author_image" id="author-image" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+    </div>
+
     
     <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
 </form>
